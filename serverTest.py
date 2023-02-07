19c954cdde5b83b389cdd4206a2941e199bf9c09
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2023-02-07 15:29:47 -0800

    Update serverTest.py

--- serverTest.py
+++ serverTest.py
@@ -4,7 +4,7 @@
 import socket
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Assigns a port for the server that listens to clients connecting to this port.
-serv.bind((’0.0.0.0’, 8080))
+serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
 while True:
 conn, addr = serv.accept()
@@ -12,7 +12,7 @@ from_client = ’’
 while True:
 data = conn.recv(4096)
 if not data: break
-from_client += data.decode(’utf_8’)
+from_client += data.decode('utf_8')
 print(from_client)
 conn.send("I am SERVER\n".encode())
 conn.close()
