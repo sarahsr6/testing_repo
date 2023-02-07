1773ac4ebd41eff43e79555d7f5150e8910afba3
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2023-02-07 15:10:05 -0800

    Update clientTest.py

--- clientTest.py
+++ clientTest.py
@@ -1,6 +1,6 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((’YOUR_SERVER_IP’, 8080))
+client.connect((’131.179.14.35’, 8080))
 client.sendall(’I am CLIENT\n’.encode())
 from_server = client.recv(4096)
 client.close()
