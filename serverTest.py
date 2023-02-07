2a3329f2539fd47906841fc42a58666e3f91419e
sarahsr6 <44517549+sarahsr6@users.noreply.github.com>
2023-02-07 15:28:27 -0800

    Update serverTest.py

--- serverTest.py
+++ serverTest.py
@@ -1,6 +1,6 @@
 # Reminder: This is a comment. The first line imports a default library "socket" into Python.
 # You dont install this. The second line is initialization to add TCP/IP protocol to the
-endpoint.
+
 import socket
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Assigns a port for the server that listens to clients connecting to this port.
